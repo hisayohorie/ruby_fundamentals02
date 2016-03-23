0ed768b62289eb000f4557c0cbf7b76069f2a95b
HISAYO <hisayohorie@gmail.com>
2016-03-23 19:23:00 -0400

    hash01

---
+++ exercise07.rb
@@ -0,0 +1,12 @@
+$students = {
+  :cohort1 => 34,
+  :cohort2 => 42,
+  :cohort3 => 22
+}
+
+def student_number
+  $students.each do |cohort, number|
+    puts "#{cohort}: #{number}  students"
+  end
+end
+student_number
