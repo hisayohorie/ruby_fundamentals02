f2c68829d3c7bed71a286c7f8cbb83739d8d0d9e
HISAYO <hisayohorie@gmail.com>
2016-03-23 21:13:58 -0400

    hash03

--- exercise07.rb
+++ exercise07.rb
@@ -15,3 +15,7 @@ student_number
 $students.each do|cohort,number|
   puts "#{cohort}"
 end
+$students.update($students){|cohort, number| number * 1.05.round}
+puts $students
+$student.delete(:cohort2)
+puts $students
